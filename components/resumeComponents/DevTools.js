521999232bb559e946a7068bc35964eae9773f06
godspower2020 <augustinegodspower40@gmail.com>
2023-03-21 06:19:45 -0700

    fixed resume and index

--- components/resumeComponents/DevTools.js
+++ components/resumeComponents/DevTools.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import{ urlFor } from '../../lib/sanityClient';
 
-const DevTools = ({devTool: {icon, name}}) => {
+const DevTools = ({devTool: {icon, name, color}}) => {
   return (
     <div className='dev-tool'>
         <img src={urlFor(icon)} alt={name} />
+        <p style={{color: color}}>{name}</p>
     </div>
   )
 }
